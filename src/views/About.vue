67a10ccc0c3834248a4a67b62385ca653b0afddc
潘鹏 <209595108@qq.com>
2022-06-21 00:29:08 +0800

    项目初始化

---
+++ src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button @click="change">{{num}}</button>
+      {{a}}    {{b}}   {{comput}}
+  </div>
+</template>
+<script setup>
+  let num=ref(0)
+  let obj=reactive({
+      a:1,
+      b:2,
+      c:3
+  })
+  let {a,b,c}=toRefs(obj)
+  const change=()=>{
+    comput.value=11124
+      console.log(comput.value)
+  }
+  let comput=computed({
+    get(){
+        return a=789
+    },
+    set(value){
+       console.log('变化')
+    }
+  })
+</script>
+<style lang="less"></style>
